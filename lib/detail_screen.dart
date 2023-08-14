496a6170953b277a0cdc2ac647c40a8d8b5636bf
madyanes <madyanes.dev@gmail.com>
2023-08-15 02:26:40 +0700

    handling overflow causes by Image widget menangani overflow yang timbul akibat widget Image yang terakhir dengan memanfaatkan widget `SingleChildScrollView`

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -7,61 +7,63 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Image.asset('images/farm-house.jpg'),
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('images/farm-house.jpg'),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8.0),
-                      Text('09:00 - 20:00'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8.0),
-                      Text('Rp 25.000'),
-                    ],
-                  ),
-                ],
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.calendar_today),
+                        SizedBox(height: 8.0),
+                        Text('Open Everyday'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.access_time),
+                        SizedBox(height: 8.0),
+                        Text('09:00 - 20:00'),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.monetization_on),
+                        SizedBox(height: 8.0),
+                        Text('Rp 25.000'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                ),
               ),
-            ),
-            Image.network(
-                'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
-          ],
+              Image.network(
+                  'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+            ],
+          ),
         ),
       ),
     );
