a46edd366b1b20963b7e593f1fc8777c88ca7660
madyanes <madyanes.dev@gmail.com>
2023-08-14 22:39:05 +0700

    centering text "Farm House Lembang" - posisi teks tidak akan terlihat berada di tengah layar secara horizontal, sebabnya, lebar kolom sama dengan lebar konten teks - Widget `CrossAxisAlignment.stretch` berfungsi untuk menetapkan posisi kolom supaya berada di tengah layar secara horizontal (x axis)

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,18 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               margin: const EdgeInsets.only(top: 16.0),
-              child: const Text('Farm House Lembang'),
+              child: const Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
