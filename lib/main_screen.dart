a4ccd2532fb17ff5d99dd9c8c4deed00b8e1102a
madyanes <madyanes.dev@gmail.com>
2023-08-16 18:39:34 +0700

    use Card widget widget material design yang menghasilkan tampilan seperti kartu dengan ujung yang membulat dan bayangan di belakang
    Catatan:
    - Aplikasi akan mengalami overflow karena aset gambar yang terlalu besar
    - Kita bisa saja mengatur tinggi gambar secara manual, namun kali ini kita akan memanfaatkan widget Expanded agar tampilan juga dapat menyesuaikan di perangkat yang lebih besar atau kecil

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,6 +9,30 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Wisata Bandung'),
       ),
+      body: Card(
+        child: Row(
+          children: <Widget>[
+            Image.asset('images/farm-house.jpg'),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Farm House Lembang',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Lembang'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
